98b56ca2ee82555431e15c3a57afa3f0a7cd84c0
Gnlow <entrydark@gmail.com>
2020-06-07 15:17:26 +0900

    typing

--- mod.ts
+++ mod.ts
@@ -3,7 +3,31 @@ import {
     name_f,
     Discrete,
 } from "./deps.ts"
+import {
+    gender,
+} from "./symbol.ts"
 
 console.log(
     new Discrete(<[string, number][]> name_m).rand(Math.random())
 )
+
+interface Kid {
+    [gender]: "m" | "f",
+}
+
+type Realize<T> = T extends keyof Kid ? Kid[T] : unknown
+
+type RealizeMap<T extends Symbol[]> = {
+    [K in keyof T]: Realize<T[K]>
+}
+
+class Distribution<T, R extends Symbol[]> {
+    constructor(
+        gen: (seed: number, ...val: RealizeMap<R>) => T,
+        ...require: R
+    ){
+
+    }
+}
+
+new Distribution((seed, gender) => 0, gender)
