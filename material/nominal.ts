c0cedfb51ec2245dc78ecb02a143b54d3bf9c625
Gnlow <entrydark@gmail.com>
2020-07-12 22:32:50 +0900

    skip column names

--- material/nominal.ts
+++ material/nominal.ts
@@ -2,13 +2,14 @@ import read from "./read.ts"
 
 export default read(async CSV => {
     const result: string[][] = [];
+
+    CSV[Symbol.asyncIterator]().next() // Skip column names
+
     for await (const row of CSV) {
         result.push([])
         for await (const cell of row) {
             result[result.length - 1].push(cell.trim())
         }
     }
-    const [key, ...data] = result
-
-    return data
+    return result
 })
