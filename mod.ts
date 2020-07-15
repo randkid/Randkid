1404ec6faf19e4802e2cb7134f8bacec96d1036a
Gnlow <entrydark@gmail.com>
2020-07-15 23:05:01 +0900

    more export

--- mod.ts
+++ mod.ts
@@ -1,1 +1,8 @@
 export {default as weightedList} from "./material/weightedList.ts"
+export {
+    Material,
+    Categorical,
+    Nominal,
+    Numerical,
+    ComplexNumerical,
+} from "./material/Material.ts"
