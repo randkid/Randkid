e2ed146785b2cbf15abd88ab3d9c6087a974f79b
Gnlow <entrydark@gmail.com>
2020-07-18 01:36:30 +0900

    add ComplexNumerical

---
+++ material/ComplexNumerical.ts
@@ -0,0 +1,13 @@
+import Material, {Arg as MaterialArg} from "./Material.ts"
+
+export interface Arg<T, I extends Material<any, any[]>[]> extends MaterialArg<T, I> {
+    ranges: Record<string, [number, number]>
+}
+
+export default class ComplexNumerical<T, I extends Material<any, any[]>[]> extends Material<T, I> {
+    ranges: Record<string, [number, number]> = {}
+    constructor({inputMaterials, rand, ranges}: Arg<T, I>){
+        super({inputMaterials, rand})
+        this.ranges = ranges
+    }
+}
