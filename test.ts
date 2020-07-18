e5597ee0f601d1fdb3d883b5ee5cfe8e46bbb9e0
Gnlow <entrydark@gmail.com>
2020-07-18 12:16:13 +0900

    delete test files

--- test.ts
+++
@@ -1,9 +0,0 @@
-import { weightedList } from "./mod.ts"
-import { Nominal } from "./material/Material.ts"
-
-console.log((await weightedList("./bloodType.csv")).rand(Math.random()))
-
-console.log(new Nominal({
-    inputMaterials: [await weightedList("./bloodType.csv")],
-    rand: (seed, bloodType) => bloodType
-}).rand(Math.random()))
