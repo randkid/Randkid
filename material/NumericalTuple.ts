d5885415dde6830d56efbbf0649685347af87e72
Gnlow <entrydark@gmail.com>
2020-07-24 22:55:58 +0900

    change name "ComplexNumerical" -> "NumericalTuple"

--- material/NumericalTuple.ts
+++ material/NumericalTuple.ts
@@ -4,7 +4,7 @@ export interface Arg<T, I extends Material<any, any[]>[]> extends MaterialArg<T,
     ranges: Record<string, [number, number]>
 }
 
-export default class ComplexNumerical<T, I extends Material<any, any[]>[]> extends Material<T, I> {
+export default class NumericalTuple<T, I extends Material<any, any[]>[]> extends Material<T, I> {
     ranges: Record<string, [number, number]>
     constructor({inputMaterials, rand, ranges}: Arg<T, I>){
         super({inputMaterials, rand})
