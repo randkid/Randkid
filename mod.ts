0d805eeaf142078c69c74073bc03fb590590fb55
Gnlow <entrydark@gmail.com>
2020-09-09 13:23:21 +0900

    add tuple()

--- mod.ts
+++ mod.ts
@@ -3,3 +3,5 @@ export {default as Categorical} from "./material/Categorical.ts"
 export {default as Nominal} from "./material/Nominal.ts"
 export {default as Numerical} from "./material/Numerical.ts"
 export {default as NumericalTuple} from "./material/NumericalTuple.ts"
+
+export {default as tuple} from "./tuple.ts"
