6df3bb2f8f048dd0d66dc507c0133777b9503eec
Gnlow <entrydark@gmail.com>
2020-09-15 21:59:36 +0900

    Nominal - delete "extends string"

--- material/Nominal.ts
+++ material/Nominal.ts
@@ -1,10 +1,10 @@
 import Material from "./Material.ts"
 import Categorical, {Arg as CategoricalArg} from "./Categorical.ts"
 
-export interface Arg<T extends string, I extends Material<any, any[]>[]> extends CategoricalArg<T, I> {
+export interface Arg<T, I extends Material<any, any[]>[]> extends CategoricalArg<T, I> {
 }
 
-export default class Nominal<T extends string, I extends Material<any, any[]>[]> extends Categorical<T, I> {
+export default class Nominal<T, I extends Material<any, any[]>[]> extends Categorical<T, I> {
     constructor(args: Arg<T, I>){
         super(args)
     }
